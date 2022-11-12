8b7a968d158c3b19dc913aaaed184caceda0e5fe
AlexusVulgaris <115590647+AlexusVulgaris@users.noreply.github.com>
2022-11-12 11:37:23 +0300

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+    if index is None:
+        list_3 = list_[:-1]
+    else:
+        list_1 = list_[:index]
+        list_2 = list_[index+1:]
+        list_3 = list_1 + list_2
+    return list_3  # TODO реализовать функцию удаления элемента из списка по индексу
+
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
